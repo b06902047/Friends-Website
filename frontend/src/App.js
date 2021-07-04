d2e6b427256a31ea87dd18889185e8195aa15b48
YenChen <b06902047@ntu.edu.tw>
2021-07-04 19:54:27 +0800

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Drawer from "./Components/Drawer.js"
+
+function App() {
+  return (
+    <Drawer
+          />
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/Appbar.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import ListIcon from '@material-ui/icons/List';
+import { useState } from 'react'
+import Grid from '@material-ui/core/Grid';
+import {
+  Link
+} from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    marginTop: '10%',
+    fontFamily: 'fantasy',
+    fontStyle: 'bold',
+    fontDisplay: 'swap',
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  font: {
+    marginTop: '15%',
+    fontFamily: 'fantasy',
+    fontStyle: 'normal',
+    fontDisplay: 'swap',
+    fontSize: 20,
+    color: "white",
+    "&:hover": {
+      color: "red",
+    },
+  },
+  visit: {
+    marginTop: '10%',
+    fontFamily: 'fantasy',
+    fontStyle: 'normal',
+    fontDisplay: 'swap',
+    fontSize: 20,
+    marginLeft: '50%',
+    color: "white",
+  }
+}));
+
+export default function ButtonAppBar(props) {
+  const classes = useStyles();
+  const [count, setCount] = useState(0)
+  const username = "All";
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" style={{ background: '#ff9800' }}>
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="icon">
+            <ListIcon />
+          </IconButton>
+          <Grid container className={classes.root}>
+                  <Grid item xs={4}>
+                    <h3 className={classes.title}>
+                      Happy Website
+                    </h3>
+                  </Grid>
+                  <Grid item xs={2}>
+                    <h4 className={classes.font}>Home</h4>
+                  </Grid>
+                  <Grid item xs={2}>
+                    <h4 className={classes.font}>Chat</h4>
+                  </Grid>
+                  <Grid item xs={2}>
+                    <h4 className={classes.font}>Calender</h4>
+                  </Grid>
+                  <Grid item xs={2}>
+                    {props.loggedIn===false?
+                      <h4 className={classes.font}>Login</h4>
+                      :<h4 className={classes.font}>{username}</h4>
+                    }
+                  </Grid>
+          </Grid>
+        </Toolbar>
+        
+      </AppBar>
+    </div>
+  );
+}
